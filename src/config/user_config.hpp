9d29afb518b8a4f8843221e04256068e66ea338c
auria.mg <auria.mg@gmail.com>
2017-02-20 19:14:33 -0500

    Add config flag to persist GUI visibility state at samuncle's request

--- src/config/user_config.hpp
+++ src/config/user_config.hpp
@@ -916,6 +916,10 @@ namespace UserConfigParams
             PARAM_DEFAULT( BoolUserConfigParam(false, "artist_debug_mode",
                                "Whether to enable track debugging features") );
 
+    PARAM_PREFIX BoolUserConfigParam        m_hide_gui
+        PARAM_DEFAULT(BoolUserConfigParam(false, "debug_hide_gui",
+            "Whether to hide the GUI (artist debug mode)"));
+
     PARAM_PREFIX BoolUserConfigParam        m_everything_unlocked
             PARAM_DEFAULT( BoolUserConfigParam(false, "everything_unlocked",
                                "Enable all karts and tracks") );
